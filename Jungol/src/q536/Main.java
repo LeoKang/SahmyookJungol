a4f58fafbb8c7bb6c5f341b61cdf7d2b79397f27
SYU <SYU@DESKTOP-1VHBQ5H>
2024-12-31 09:17:22 +0900

    536

---
+++ Jungol/src/q536/Main.java
@@ -0,0 +1,19 @@
+package q536;
+
+public class Main {
+	public static void main(String[] args) {
+		// 1. for
+		for (int i = 1; i <= 15; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		// 2. while
+		int j = 1;
+		while(j <= 15) {
+			System.out.print(j++ + " ");
+		}
+		
+		// 3. do ~ while
+	}
+}
