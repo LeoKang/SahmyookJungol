74347c9993468781c9b09fd197eed25f673971cf
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-06 09:41:10 +0900

    529

---
+++ Jungol/src/q529/Main.java
@@ -0,0 +1,20 @@
+package q529;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int height = sc.nextInt();
+		int weight = sc.nextInt();
+		sc.close();
+
+//		System.out.println(height + " " + weight);
+
+		int res = weight + 100 - height;
+		System.out.println(res);
+		if (res > 0) {
+			System.out.println("Obesity");
+		}
+	}
+}
