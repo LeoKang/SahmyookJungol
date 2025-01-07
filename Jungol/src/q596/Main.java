ba004c8822ef284ba839dff78a42c7bd06dd5ee7
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-07 17:11:31 +0900

    596

---
+++ Jungol/src/q596/Main.java
@@ -0,0 +1,23 @@
+package q596;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int len = sc.nextInt();
+		sc.close();
+
+		System.out.println(str + " " + len);
+
+		if (str.length() > len) {
+			for (int i = str.length() - 1; len > 0; i--, len--) {
+				System.out.print(str.charAt(i));
+			}
+		} else {
+			StringBuilder reversed = new StringBuilder(str).reverse();
+			System.out.println(reversed);
+		}
+	}
+}
