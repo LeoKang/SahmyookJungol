b331e39c19093ad9ad5fb9e60f7723ce30a55050
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-09 09:18:41 +0900

    105, 514

--- Jungol/src/q105/Main.java
+++ Jungol/src/q105/Main.java
@@ -1,10 +1,33 @@
 package q105;
 
+import java.text.DecimalFormat;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("문제풀이.....");
+		info[] arr = new info[5];
+		arr[0] = new info("Seoul", 10312545, "+91,375");
+		arr[1] = new info("Pusan", 3567910, "+5,868");
+		arr[2] = new info("Incheon", 2758296, "+64,888");
+		arr[3] = new info("Daegu", 2511676, "+17,230");
+		arr[4] = new info("Gwangju", 1454636, "+29,774");
+		DecimalFormat decFormat = new DecimalFormat("###,###");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.printf("%15s", arr[i].city);
+			System.out.printf("%15s", decFormat.format(arr[i].people));
+			System.out.printf("%15s", arr[i].updown);
+			System.out.println();
+		}
+	}
 
-		System.out.println("충돌");
-		System.out.println("1234");
+	public static class info {
+		String city;
+		int people;
+		String updown;
+
+		info(String city, int people, String updown) {
+			this.city = city;
+			this.people = people;
+			this.updown = updown;
+		}
 	}
 }
