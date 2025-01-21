e0385b1018795768e768f2d3d8314b2650f5176d
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-21 09:28:37 +0900

    572

---
+++ Jungol/src/q572/Main.java
@@ -0,0 +1,22 @@
+package q572;
+
+import java.util.Scanner;
+
+public class Main {
+	public double getArea(int p) {
+		double ret = p * p * 3.14;
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		sc.close();
+
+//		System.out.println(r);
+
+		Main m = new Main();
+		double ret = m.getArea(r);
+		System.out.printf("%.2f\n", ret);
+	}
+}
